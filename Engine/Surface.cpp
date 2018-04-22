0ebe1b88c5a37535338bd35ffbe6145e9dc19ad4
CalebADB <cbary3@gmail.com>
2018-04-22 19:22:57 -0400

    Defined Blit

--- Engine/Surface.cpp
+++ Engine/Surface.cpp
@@ -92,7 +92,19 @@ Surface::~Surface()
 
 void Surface::blit(const Surface & source_surface, const Rect & destination)
 {
-
+	Coordinates2D<int> setPxlI = destination.getTopLeft();
+	Dimensions2D<int> setDimensions = destination.getDimension();
+	
+	Coordinates2D<int> getPxlI;
+	for (getPxlI.y = 0; getPxlI.y < setDimensions.height; getPxlI.y++)
+	{
+		setPxlI.y++;
+		for (getPxlI.x = 0; getPxlI.x < setDimensions.width; getPxlI.x++)
+		{
+			setPxlI.x++;
+			setPxl(setPxlI, source_surface.getPxl(getPxlI));
+		}
+	}
 }
 
 void Surface::setPxl(const Coordinates2D<int>& coordinate, const Color & pxlColor)
